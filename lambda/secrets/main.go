7c70af05d3e33a5e61fc91a67832a2124f2ab5aa
Gurunandan Bhat <gbhat@pobox.com>
2025-08-18 19:13:23 +0530

    First Cut of AWS HTTP API backed by a Trivial Lambda

---
+++ lambda/secrets/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
+)
+
+var smClient *secretsmanager.Client
+
+func init() {
+	config, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create Secrets Manager client
+	smClient = secretsmanager.NewFromConfig(config)
+}
+
+func handler(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	secretName := "mario/defaultSecret"
+	input := &secretsmanager.GetSecretValueInput{
+		SecretId:     aws.String(secretName),
+		VersionStage: aws.String("AWSCURRENT"),
+	}
+
+	result, err := smClient.GetSecretValue(context.TODO(), input)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusInternalServerError,
+			Body:       err.Error(),
+		}, err
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+		Body:       *result.SecretString,
+	}, nil
+}
+
+func main() {
+
+	lambda.Start(handler)
+}
--- mario-cdk.go
+++ mario-cdk.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"os"
+
 	"github.com/aws/aws-cdk-go/awscdk/v2"
-	// "github.com/aws/aws-cdk-go/awscdk/v2/awssqs"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsapigatewayv2"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsapigatewayv2authorizers"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsapigatewayv2integrations"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awscognito"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsiam"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awslambda"
 	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
 )
@@ -19,11 +26,46 @@ func NewMarioCdkStack(scope constructs.Construct, id string, props *MarioCdkStac
 	stack := awscdk.NewStack(scope, &id, &sprops)
 
 	// The code that defines your stack goes here
+	loginLambda := awslambda.NewFunction(stack, jsii.String("MarioCDKLambdaLoginURL"), &awslambda.FunctionProps{
+		Runtime: awslambda.Runtime_PROVIDED_AL2023(),
+		Code:    awslambda.AssetCode_FromAsset(jsii.String("lambda/secrets/function.zip"), nil),
+		Handler: jsii.String("main"),
+	})
+	loginLambda.Role().AddManagedPolicy(awsiam.ManagedPolicy_FromAwsManagedPolicyName(jsii.String("SecretsManagerReadWrite")))
 
-	// example resource
-	// queue := awssqs.NewQueue(stack, jsii.String("MarioCdkQueue"), &awssqs.QueueProps{
-	// 	VisibilityTimeout: awscdk.Duration_Seconds(jsii.Number(300)),
-	// })
+	marioAuth := awsapigatewayv2authorizers.NewHttpUserPoolAuthorizer(
+		jsii.String("MarioAuth"),
+		awscognito.UserPool_FromUserPoolId(stack, jsii.String("MarioAuth"), jsii.String("ap-south-1_BoauKNKc9")),
+		nil,
+	)
+
+	loginAPI := awsapigatewayv2.NewHttpApi(stack, jsii.String("LoginAPI"), &awsapigatewayv2.HttpApiProps{
+		ApiName: jsii.String("LambdaLoginAPI"),
+		CorsPreflight: &awsapigatewayv2.CorsPreflightOptions{
+			AllowHeaders: jsii.Strings("Authorization"),
+			AllowMethods: &[]awsapigatewayv2.CorsHttpMethod{
+				awsapigatewayv2.CorsHttpMethod_GET,
+				awsapigatewayv2.CorsHttpMethod_POST,
+				awsapigatewayv2.CorsHttpMethod_OPTIONS,
+				awsapigatewayv2.CorsHttpMethod_HEAD,
+			},
+			AllowOrigins: jsii.Strings("*"),
+		},
+		DefaultAuthorizer:          marioAuth,
+		DefaultAuthorizationScopes: jsii.Strings("openid", "email"),
+	})
+
+	loginIntegration := awsapigatewayv2integrations.NewHttpLambdaIntegration(
+		jsii.String("LoginIntegration"),
+		loginLambda,
+		&awsapigatewayv2integrations.HttpLambdaIntegrationProps{},
+	)
+
+	loginAPI.AddRoutes(&awsapigatewayv2.AddRoutesOptions{
+		Path:        jsii.String("/secret/{id}"),
+		Methods:     &[]awsapigatewayv2.HttpMethod{awsapigatewayv2.HttpMethod_GET},
+		Integration: loginIntegration,
+	})
 
 	return stack
 }
@@ -49,7 +91,7 @@ func env() *awscdk.Environment {
 	// Account/Region-dependent features and context lookups will not work, but a
 	// single synthesized template can be deployed anywhere.
 	//---------------------------------------------------------------------------
-	return nil
+	// return nil
 
 	// Uncomment if you know exactly what account and region you want to deploy
 	// the stack to. This is the recommendation for production stacks.
@@ -63,8 +105,8 @@ func env() *awscdk.Environment {
 	// implied by the current CLI configuration. This is recommended for dev
 	// stacks.
 	//---------------------------------------------------------------------------
-	// return &awscdk.Environment{
-	//  Account: jsii.String(os.Getenv("CDK_DEFAULT_ACCOUNT")),
-	//  Region:  jsii.String(os.Getenv("CDK_DEFAULT_REGION")),
-	// }
+	return &awscdk.Environment{
+		Account: jsii.String(os.Getenv("CDK_DEFAULT_ACCOUNT")),
+		Region:  jsii.String(os.Getenv("CDK_DEFAULT_REGION")),
+	}
 }
